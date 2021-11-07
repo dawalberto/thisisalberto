77d745079b3b566f47db030f17ea721f1c8149cb
dawalberto <albertoinformatica97@gmail.com>
2021-11-07 14:36:44 +0000

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,14 @@
 <template>
-  <nav class="shadow-lg">
-    <div class="max-w-6xl mx-auto px-4 sm:px-16 flex justify-between">
-      <div class="flex space-x-7">
-        <!-- Website Logo -->
-        <div class="my-2">
-          <a href="#" class="flex items-center">
-            <img src="@/assets/i.png" alt="Logo" class="w-28 sm:w-36" />
-          </a>
-        </div>
-        <!-- Primary Navbar items -->
-        <div class="flex items-center space-x-1">
-          <a
-            href=""
-            class="
-              text-4xl
-              md:text-lg
-              opacity-80
-              cursor-pointer
-              py-4
-              px-2
-              text-gray-600
-              font-semibold
-              hover:text-yellow-500
-              transition
-              duration-300
-            "
-          >
-            {{ windowWidth >= 768 ? 'Projects' : '🧑🏻‍💻' }}
-          </a>
-          <a
-            href=""
-            class="
-              text-4xl
-              md:text-lg
-              opacity-80
-              cursor-pointer
-              py-4
-              px-2
-              text-gray-600
-              font-semibold
-              hover:text-yellow-500
-              transition
-              duration-300
-            "
-          >
-            {{ windowWidth >= 768 ? 'Studies' : '🎓' }}
-          </a>
-        </div>
-      </div>
-      <!-- Secondary Navbar items -->
-      <div class="flex items-center space-x-3">
-        <a
-          href=""
-          class="
-            text-4xl
-            md:text-lg
-            opacity-80
-            cursor-pointer
-            py-4
-            px-2
-            text-gray-600
-            font-semibold
-            hover:text-yellow-500
-            transition
-            duration-300
-          "
-        >
-          {{ windowWidth >= 768 ? 'Contact' : '📞' }}
-        </a>
-      </div>
-    </div>
-  </nav>
+  <menu-bar />
   <router-view />
 </template>
 
 <script>
-import { ref } from 'vue'
-import { useWindowSize } from 'vue-window-size'
+import MenuBar from '@/components/MenuBar.vue'
 
 export default {
-  setup() {
-    let windowWidth = ref(useWindowSize().width)
-
-    let projectsTitle = ref('')
-    let studiesTitle = ref('')
-    let contactTitle = ref('')
-
-    projectsTitle = windowWidth.value >= 768 ? 'Projects' : '🧑🏻‍💻'
-    studiesTitle = windowWidth.value >= 768 ? 'Studies' : '🎓'
-    contactTitle = windowWidth.value >= 768 ? 'Contact' : '📞'
-
-    return { projectsTitle, studiesTitle, contactTitle, windowWidth }
-  },
+  name: 'App',
+  components: { MenuBar },
 }
 </script>
 
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav class="shadow-none">
+    <div class="max-w-6xl mx-auto px-4 sm:px-16 flex justify-between">
+      <div class="flex sm:space-x-9">
+        <!-- Website Logo -->
+        <div class="my-2">
+          <a href="#" class="flex items-center">
+            <img src="@/assets/i.png" alt="Logo" class="w-28 sm:w-36" />
+          </a>
+        </div>
+        <!-- Primary Navbar items -->
+        <div class="flex items-center space-x-4">
+          <a href="" class="menu-item">
+            {{ projectsTitle }}
+          </a>
+          <a href="" class="menu-item">
+            {{ studiesTitle }}
+          </a>
+        </div>
+      </div>
+      <!-- Secondary Navbar items -->
+      <div class="flex items-center space-x-3">
+        <a href="" class="menu-item">
+          {{ contactTitle }}
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+import { useWindowSize } from 'vue-window-size'
+
+export default {
+  name: 'MenuBar',
+  setup() {
+    let windowWidth = ref(useWindowSize().width)
+
+    const projectsTitle = computed(() => {
+      return windowWidth.value >= 768 ? 'Projects' : '🧑🏻‍💻'
+    })
+    const studiesTitle = computed(() => {
+      return windowWidth.value >= 768 ? 'Studies' : '🎓'
+    })
+    const contactTitle = computed(() => {
+      return windowWidth.value >= 768 ? 'Contact' : '📞'
+    })
+
+    return { projectsTitle, studiesTitle, contactTitle }
+  },
+}
+</script>
+
+<style>
+.menu-item {
+  @apply text-4xl md:text-lg opacity-80 
+  cursor-pointer py-4 px-2 
+  text-gray-600 font-semibold hover:text-yellow-500 
+  transition duration-300;
+}
+</style>
